85bd3624e3932bb17715b48e01fbccb6e9afabc6
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2023-04-27 20:27:39 +0700

    created new fields with powers of ten

--- src/main/java/oop/diskUsage/FileSizeConverter.java
+++ src/main/java/oop/diskUsage/FileSizeConverter.java
@@ -3,21 +3,27 @@ package oop.diskUsage;
 import java.text.DecimalFormat;
 
 public class FileSizeConverter {
-    public static String print(long size){
+
+    private static final double THIRD_POWER_OF_TEN = Math.pow(10, 3);
+    private static final double SIXTH_POWER_OF_TEN = Math.pow(10, 6);
+    private static final double NINTH_POWER_OF_TEN = Math.pow(10, 6);
+    private static final double TWELFTH_POWER_OF_TEN = Math.pow(10, 12);
+
+    public static String print(long size) {
 
         DecimalFormat df = new DecimalFormat("#.###");
 
-        if (size < Math.pow(10,3)){
+        if (size < THIRD_POWER_OF_TEN) {
             return "[" + size + " B]";
         }
-        if (size<Math.pow(10,6)){
-            return "[" + df.format(size / Math.pow(10,3)) + " kB]";
+        if (size < SIXTH_POWER_OF_TEN) {
+            return "[" + df.format(size / THIRD_POWER_OF_TEN) + " kB]";
         }
-        if (size<Math.pow(10,9)){
-            return "[" + df.format(size / Math.pow(10,6)) + " MB]";
+        if (size < NINTH_POWER_OF_TEN) {
+            return "[" + df.format(size / SIXTH_POWER_OF_TEN) + " MB]";
         }
-        if (size<Math.pow(10,12)){
-            return "[" + df.format(size / Math.pow(10,9)) + " GB]";
+        if (size < TWELFTH_POWER_OF_TEN) {
+            return "[" + df.format(size / NINTH_POWER_OF_TEN) + " GB]";
         }
         return " ";
     }
